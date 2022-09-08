6eb59a27f83a84cabee25ae6d2532feb2a76d7b0
Esteban Duarte <eduart73.eadp@gmail.com>
2022-09-08 15:39:10 -0500

    Adding persons name

--- how_old_are_you.sh
+++ how_old_are_you.sh
@@ -3,13 +3,14 @@
 
 #¿how old are you?
 
-read -p "Enter you year of birth: " YEAR
-read -p "Enter you month of birth: " MONTH
-read -p "Enter you day of birth: " DAY
+read -p "Enter your name: " NAME
+read -p "Enter your year of birth: " YEAR
+read -p "Enter your month of birth: " MONTH
+read -p "Enter your day of birth: " DAY
 EDAD=$(($(date +%Y)-$YEAR))
 if [ $(date +%m) -lt $MONTH ]; then
 	EDAD=$(($EDAD-1))
 elif [ $(date +%m) -eq $MONTH -a $(date +%d) -lt $DAY ]; then
 	EDAD=$(($EDAD-1))
 fi
-echo Tienes $EDAD years
+echo $NAME","  tienes $EDAD years
